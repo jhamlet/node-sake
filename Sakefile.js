9544290ee7162221ff30ab037e24a99fea2f39d4
Jerry Hamlet <jerry@hamletink.com>
2012-04-30 21:58:43 -0700

    FileList working properly...

--- Sakefile.js
+++ Sakefile.js
@@ -1,6 +1,7 @@
 
 var Path    = require("path"),
-    futil   = require("sake/file-utils")
+    futil   = require("sake/file-utils"),
+    files   = new FileList("**/*", "some/non-existing/file.js")
 ;
 
 require("sake/clean");
