4281b63f4fd8abe22e7dc74f5ec1ec821fb4ced1
Jerry Hamlet <jerry@hamletink.com>
2012-05-01 11:12:46 -0700

    Moving things around (everything in the lib/node_modules/sake directory now.)
    Updated listing of tasks and their prerequisites with a bit more detail.

--- Sakefile.js
+++ Sakefile.js
@@ -7,21 +7,24 @@ var Path    = require("path"),
 require("sake/clean");
 require("sake/clobber");
 
+desc("Sample task one.");
 task("one", ["two"], function (t) {
     // task actions
     t.done();
 });
 
+desc("Sample task two.");
 task("two");
 
-desc("Test text file");
+desc("Test text file.");
 file("test.txt", ["LICENSE"], function (t) {
     write(t.name, read(t.prerequisites[0], "utf8"), "utf8");
 });
 
+desc("Default task.");
 task("default", ["test.txt"]);
 
 // console.log(task("one").prerequisites.length);
 // console.log(Object.keys(Task.tasks));
-// console.log(Task.tasks);
+// console.log(Task.getAll());
 // console.log(Task.get("one") + "");
