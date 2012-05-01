5fc82bdac13e0ebe3daf1eb7b1670f0f0235ad6f
Jerry Hamlet <jerry@hamletink.com>
2012-05-01 13:30:01 -0700

    A couple of Sync tasks.

--- Sakefile.js
+++ Sakefile.js
@@ -14,15 +14,27 @@ task("one", ["two"], function (t) {
 });
 
 desc("Sample task two.");
-task("two");
+task("two", function (t) {
+    t.done();
+});
 
 desc("Test text file.");
 file("test.txt", ["LICENSE"], function (t) {
+    console.log(t.name);
     write(t.name, read(t.prerequisites[0], "utf8"), "utf8");
+    t.done();
 });
 
 desc("Default task.");
 task("default", ["test.txt"]);
+
+taskSync("testSync", function (t) {
+    console.log(t.name);
+});
+
+taskSync("testSync2", ["testSync"], function (t) {
+    console.log(t.name);
+});
 
 // console.log(task("one").prerequisites.length);
 // console.log(Object.keys(Task.tasks));
