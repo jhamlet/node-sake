db610ec1776aee1888891c480f8a14d40b52d685
Jerry Hamlet <jerry@hamletink.com>
2012-05-01 14:59:58 -0700

    Start using the new sake to build things.

--- Sakefile.js
+++ Sakefile.js
@@ -1,45 +1,33 @@
-
+/*globals desc, task, taskSync, file, directory, fileSync, FileList, CLOBBER, CLEAN, write, read, log */
 var Path    = require("path"),
     futil   = require("sake/file-utils"),
-    files   = new FileList("**/*", "some/non-existing/file.js")
+    files   = new FileList("**/*", "some/non-existing/file.js"),
+    authorInfo
 ;
 
-require("sake/clean");
-require("sake/clobber");
-
-desc("Sample task one.");
-task("one", ["two"], function (t) {
-    // task actions
-    t.done();
+desc("Keep the LICENSE file up-to-date.");
+taskSync("LICENSE", ["read-authors"], function (t) {
+    var year = new Date().getFullYear(),
+        license = read(t.name, "utf8").split("\n")
+    ;
+    
+    license[0] = "Copyright (c) " + year + " " + authorInfo;
+    
+    write(t.name, license.join("\n"), "utf8");
+    log(t.name + " updated");
 });
 
-desc("Sample task two.");
-task("two", function (t) {
-    t.done();
+taskSync("read-authors", ["AUTHORS"], function (t) {
+    authorInfo = read(t.prerequisites[0], "utf8").split("\n")[0];
+    log("Read author info");
 });
 
-directory("tmp/path/directory");
-CLOBBER.include("tmp");
-
-desc("Test text file.");
-fileSync("tmp/path/directory/test.txt", ["tmp/path/directory", "LICENSE"], function (t) {
-    console.log(t.name);
-    write(t.name, read(t.prerequisites[1], "utf8"), "utf8");
-});
-CLEAN.include("tmp/path/directory/test.txt");
-
-desc("Default task.");
-task("default", ["tmp/path/directory/test.txt"]);
-
-taskSync("testSync", function (t) {
-    console.log(t.name);
+taskSync("pre-build", function (t) {
+    log("Starting build...");
 });
 
-taskSync("testSync2", ["testSync"], function (t) {
-    console.log(t.name);
+taskSync("build", ["pre-build", "LICENSE"], function (t) {
+    log("Build complete");
 });
 
-// console.log(task("one").prerequisites.length);
-// console.log(Object.keys(Task.tasks));
-// console.log(Task.getAll());
-// console.log(Task.get("one") + "");
+taskSync("default", ["build"]);
