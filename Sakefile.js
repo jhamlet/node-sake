5ea35d5652786eae25cf8e61b643a0d71c7e9bf2
Jerry Hamlet <jerry@hamletink.com>
2012-05-01 21:22:41 -0700

    Update build.

--- Sakefile.js
+++ Sakefile.js
@@ -1,12 +1,19 @@
 /*globals desc, task, taskSync, file, directory, fileSync, FileList, CLOBBER, CLEAN, write, read, log */
-var Path = require("path"),
-    authorInfo,
-    buildStart,
+var Path        = require("path"),
+    buildStart  = new Date(),
+    authorInfo  = read("AUTHORS", "utf8").split("\n")[0],
+    readMeFiles = new FileList(),
     buildComplete
 ;
 
+/**
+ * Defines the CLEAN file-list and the 'clean' task
+ */
+require("sake/clean");
+CLEAN.include("README.md");
+
 desc("Keep the LICENSE file up-to-date.");
-taskSync("LICENSE", ["read-authors"], function (t) {
+fileSync("LICENSE", ["AUTHORS"], function (t) {
     var year = new Date().getFullYear(),
         license = read(t.name, "utf8").split("\n")
     ;
@@ -17,24 +24,28 @@ taskSync("LICENSE", ["read-authors"], function (t) {
     log(t.name + " updated");
 });
 
-taskSync("read-authors", ["AUTHORS"], function (t) {
-    authorInfo = read(t.prerequisites[0], "utf8").split("\n")[0];
-    log("Read author info");
-});
-
 taskSync("pre-build", function (t) {
-    buildStart = Date.now();
-    log("Starting build...");
+    log("Starting build at " + buildStart);
 });
 
-taskSync("build", ["pre-build", "LICENSE"], function (t) {
+taskSync("build", ["pre-build"], function (t) {
     var delta;
     
-    buildComplete = Date.now();
-    delta = buildComplete - buildStart;
+    buildComplete = new Date();
+    delta = buildComplete.getTime() - buildStart.getTime();
     
-    log("Build complete");
+    log("Build complete at " + buildComplete);
     log(delta + " ms");
 });
 
-taskSync("default", ["build"]);
+readMeFiles.include(
+    "README.tmpl", "package.json", "lib/node_modules/sake/options.js",
+    "LICENSE", "AUTHORS", "Sakefile.*"
+);
+
+fileSync("README.md", readMeFiles, function (t) {
+    log("Update " + t.name);
+});
+
+task("build", ["LICENSE", "README.md"]);
+task("default", ["build"]);
