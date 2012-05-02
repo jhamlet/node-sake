9a0e996dff2350f25bda1ddd60c4c9eeb7cadcce
Jerry Hamlet <jerry@hamletink.com>
2012-05-01 21:25:18 -0700

    Re-organize Sakefile.js.

--- Sakefile.js
+++ Sakefile.js
@@ -1,29 +1,19 @@
 /*globals desc, task, taskSync, file, directory, fileSync, FileList, CLOBBER, CLEAN, write, read, log */
 var Path        = require("path"),
-    buildStart  = new Date(),
     authorInfo  = read("AUTHORS", "utf8").split("\n")[0],
+    buildStart  = new Date(),
     readMeFiles = new FileList(),
     buildComplete
 ;
 
-/**
- * Defines the CLEAN file-list and the 'clean' task
- */
+//---------------------------------------------------------------------------
+// Defines the CLEAN file-list and the 'clean' task
+//---------------------------------------------------------------------------
 require("sake/clean");
 CLEAN.include("README.md");
-
-desc("Keep the LICENSE file up-to-date.");
-fileSync("LICENSE", ["AUTHORS"], function (t) {
-    var year = new Date().getFullYear(),
-        license = read(t.name, "utf8").split("\n")
-    ;
-    
-    license[0] = "Copyright (c) " + year + " " + authorInfo;
-    
-    write(t.name, license.join("\n"), "utf8");
-    log(t.name + " updated");
-});
-
+//---------------------------------------------------------------------------
+// Overall build tasks
+//---------------------------------------------------------------------------
 taskSync("pre-build", function (t) {
     log("Starting build at " + buildStart);
 });
@@ -37,7 +27,23 @@ taskSync("build", ["pre-build"], function (t) {
     log("Build complete at " + buildComplete);
     log(delta + " ms");
 });
-
+//---------------------------------------------------------------------------
+// LICENSE
+//---------------------------------------------------------------------------
+desc("Keep the LICENSE file up-to-date.");
+fileSync("LICENSE", ["AUTHORS"], function (t) {
+    var year = new Date().getFullYear(),
+        license = read(t.name, "utf8").split("\n")
+    ;
+    
+    license[0] = "Copyright (c) " + year + " " + authorInfo;
+    
+    write(t.name, license.join("\n"), "utf8");
+    log(t.name + " updated");
+});
+//---------------------------------------------------------------------------
+// README file
+//---------------------------------------------------------------------------
 readMeFiles.include(
     "README.tmpl", "package.json", "lib/node_modules/sake/options.js",
     "LICENSE", "AUTHORS", "Sakefile.*"
@@ -46,6 +52,8 @@ readMeFiles.include(
 fileSync("README.md", readMeFiles, function (t) {
     log("Update " + t.name);
 });
-
+//---------------------------------------------------------------------------
+// link up
+//---------------------------------------------------------------------------
 task("build", ["LICENSE", "README.md"]);
 task("default", ["build"]);
