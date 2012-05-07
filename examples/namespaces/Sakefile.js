6701445c1dd0335291341a60a33f5d0ea80359cc
Jerry Hamlet <jerry@hamletink.com>
2012-05-06 21:15:34 -0700

    Get namespace resolution working correctly and update the documentation.

--- examples/namespaces/Sakefile.js
+++ examples/namespaces/Sakefile.js
@@ -3,14 +3,27 @@ function taskAction (t) {
     log(t.namespace + " : " + t.name);
 }
 
+desc("default:default");
 taskSync("default", taskAction);
 
 namespace("foo", function () {
 
-    taskSync("default", ["default"], taskAction);
-
+    desc("foo:default");
+    taskSync("default", ["default:default"], taskAction);
+    desc("foo:not-default");
+    taskSync("not-default", taskAction);
+    desc("foo:other-task");
+    taskSync("other-task", ["not-default"], taskAction);
+    
     namespace("baz", function () {
-        taskSync("default", ["foo:default", "default"], taskAction);
+        desc("baz:not-default");
+        taskSync("not-default", taskAction);
+        desc("baz:other-task");
+        taskSync("other-task", ["not-default"], taskAction);
+        desc("baz:default");
+        taskSync("default", ["foo:default", "default:default"], taskAction);
+        desc("baz:some-task");
+        taskSync("some-task", ["other-task", "foo:other-task"]);
     });
     
 });
